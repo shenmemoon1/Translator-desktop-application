9e9129d266fda9bb6e2173b0d65a026b25dd5fa3
billlims <davidlinxf@gmail.com>
2023-09-30 14:41:44 +1000

    implement tranlate cn to en

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -91,7 +91,7 @@ namespace WindowsFormsApp1
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(770, 265);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Text = "translate";
             // 
             // Form1
             // 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 // 导入 System.Net.Http 命名空间
 using System.Net.Http;
+using Newtonsoft.Json.Linq;
 
 namespace WindowsFormsApp1
 {
@@ -20,9 +21,45 @@ namespace WindowsFormsApp1
         }
 
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
-            this.label1.Text = textBox1.Text;
+            string textToTranslate = textBox1.Text;
+            // 使用翻译服务将文本进行翻译
+            string translatedText = await TranslateTextAsync(textToTranslate);
+
+            // 将翻译后的结果显示在标签中
+            label1.Text = translatedText;
+        }
+        async Task<string> TranslateTextAsync(string text)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string apiUrl = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=" + text;
+                HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+                Console.WriteLine(response.Content);
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+
+                    // 解析 JSON 响应
+                    JArray jsonArray = JArray.Parse(jsonResponse);
+                    JArray translationArray = (JArray)jsonArray[0];
+
+                    // 提取翻译结果
+                    string translatedText = "";
+                    foreach (JArray item in translationArray)
+                    {
+                        translatedText += item[0].ToString();
+                    }
+
+                    return translatedText;
+                }
+                else
+                {
+                    return "翻译失败";
+                }
+            }
+            
         }
     }
 }
